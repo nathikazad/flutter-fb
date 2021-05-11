1ca265c6e8df58307c55bf92ec79e420fc335016
Nathik Azad <nathikazad@gmail.com>
2021-05-11 16:21:11 -0500

    all functionality working with emulator

--- flutter/lib/app/top_level_providers.dart
+++ flutter/lib/app/top_level_providers.dart
@@ -4,6 +4,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logger/logger.dart';
 import 'package:starter_architecture_flutter_firebase/services/firebase_database.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 
 final firebaseAppProvider =
     Provider<FirebaseApp>((ref) => throw UnimplementedError());
@@ -19,11 +20,12 @@ final databaseProvider = Provider<Database>((ref) {
   final app = ref.watch(firebaseAppProvider);
 
   if (auth.data?.value?.uid != null) {
+    callCloud();
     return Database(
         app: FirebaseDatabase(
             app: app,
             databaseURL:
-                'http://localhost:9000/?ns=flutter-test-5df78-default-rtdb'),
+                'http://10.0.2.2:9000/?ns=flutter-test-5df78-default-rtdb'),
         uid: auth.data!.value!.uid);
   }
   throw UnimplementedError();
@@ -35,3 +37,10 @@ final loggerProvider = Provider<Logger>((ref) => Logger(
         printEmojis: false,
       ),
     ));
+
+void callCloud() async {
+  HttpsCallable callable = FirebaseFunctions.instance.httpsCallable('listUser');
+  final results = await callable();
+  print("cloud function result");
+  print(results.data);
+}
--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,208 +1,58 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+//import 'package:auth_widget_builder/auth_widget_builder.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:starter_architecture_flutter_firebase/app/auth_widget.dart';
+import 'package:starter_architecture_flutter_firebase/app/home/home_page.dart';
+import 'package:starter_architecture_flutter_firebase/app/onboarding/onboarding_page.dart';
+import 'package:starter_architecture_flutter_firebase/app/onboarding/onboarding_view_model.dart';
+import 'package:starter_architecture_flutter_firebase/app/top_level_providers.dart';
+import 'package:starter_architecture_flutter_firebase/app/sign_in/sign_in_page.dart';
+import 'package:starter_architecture_flutter_firebase/routing/app_router.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_functions/cloud_functions.dart';
-import 'package:firebase_database/firebase_database.dart';
-// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:starter_architecture_flutter_firebase/services/shared_preferences_service.dart';
 import 'dart:io' show Platform;
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  final sharedPreferences = await SharedPreferences.getInstance();
+  final app = await Firebase.initializeApp();
+  await FirebaseAuth.instance.useEmulator('http://localhost:9099');
+  FirebaseFunctions.instance
+      .useFunctionsEmulator(origin: 'http://localhost:5001');
+  runApp(ProviderScope(
+    overrides: [
+      sharedPreferencesServiceProvider.overrideWithValue(
+        SharedPreferencesService(sharedPreferences),
+      ),
+      firebaseAppProvider.overrideWithValue(app)
+    ],
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
+    final firebaseAuth = context.read(firebaseAuthProvider);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  // FirebaseDatabase? fdb;
-  @override
-  void initState() {
-    super.initState();
-    print("init");
-    asyncMethod();
-  }
-
-  void asyncMethod() async {
-    print("init");
-    FirebaseApp app = await Firebase.initializeApp();
-    // String host = Platform.isAndroid ? '10.0.2.2:9099' : 'localhost:9099';
-    // fdb = FirebaseDatabase(
-    //     app: app,
-    //     databaseURL:
-    //         'http://localhost:9000/?ns=flutter-test-5df78-default-rtdb');
-    await FirebaseAuth.instance.useEmulator('http://localhost:9099');
-    FirebaseFunctions.instance
-        .useFunctionsEmulator(origin: 'http://localhost:5001');
-    // fdb.reference().child('/').onValue.forEach((element) {
-    //   print('new element: $element');
-    // });
-    // fdb.reference().child('/').set("hello");
-    // HttpsCallable callable =
-    //     FirebaseFunctions.instance.httpsCallable('listFruit');
-    // final results = await callable();
-    // print(results.data);
-    FirebaseAuth.instance.idTokenChanges().listen((User? user) {
-      if (user == null) {
-        print('User is currently signed out!');
-      } else {
-        print('User is signed in!');
-        print(user);
-      }
-    });
-  }
-
-  Future<UserCredential> signIn(email, password) async {
-    try {
-      UserCredential userCredential = await FirebaseAuth.instance
-          .createUserWithEmailAndPassword(email: email, password: password);
-      return userCredential;
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
-      } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
-        try {
-          UserCredential userCredential = await FirebaseAuth.instance
-              .signInWithEmailAndPassword(email: email, password: password);
-          return userCredential;
-        } on FirebaseAuthException catch (e) {
-          if (e.code == 'user-not-found') {
-            print('No user found for that email.');
-          } else if (e.code == 'wrong-password') {
-            print('Wrong password provided for that user.');
-          }
-          rethrow;
-        }
-      } else {
-        print(e);
-      }
-      rethrow;
-    } catch (e) {
-      print('sign in error');
-      print(e);
-      rethrow;
-    }
-  }
-
-  void callCloudFunction() async {
-    await FirebaseDatabase(
-            databaseURL:
-                'http://10.0.2.2:9000/?ns=flutter-test-5df78-default-rtdb')
-        .reference()
-        .child('/')
-        .set("hell");
-    // .onError((error, stackTrace) => print("db write error"));
-    HttpsCallable callable =
-        FirebaseFunctions.instance.httpsCallable('listUser');
-    final results = await callable();
-    print(results.data);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            IconButton(
-              icon: const Icon(Icons.logout),
-              tooltip: 'Logout',
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.call_end),
-              tooltip: 'Function',
-              onPressed: () {
-                callCloudFunction();
-              },
-            ),
-          ],
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      debugShowCheckedModeBanner: false,
+      home: AuthWidget(
+        nonSignedInBuilder: (_) => Consumer(
+          builder: (context, watch, _) {
+            final didCompleteOnboarding =
+                watch(onboardingViewModelProvider.state);
+            return didCompleteOnboarding ? SignInPage() : OnboardingPage();
+          },
         ),
+        signedInBuilder: (_) => HomePage(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          signIn("barry.allenn@example.com", "SuperSecretPassword!");
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      onGenerateRoute: (settings) =>
+          AppRouter.onGenerateRoute(settings, firebaseAuth),
     );
   }
 }
